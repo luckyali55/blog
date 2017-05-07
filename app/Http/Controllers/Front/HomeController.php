db1465f1b8bc97252fbaa722240e626ec192f688
lucky ali <lucky.uae1989@gmail.com>
2017-05-07 14:03:46 +0500

    init

---
+++ app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HomeController extends BaseController
+{
+    public function index(){
+        echo 'asdfa';
+    }
+}
